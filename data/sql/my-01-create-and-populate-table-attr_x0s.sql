ea2e7615387c42c21eae5465433c02f7707b377d
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2016-12-15 00:43:30 +1100

    SQL-PDF-Data: (1) Rename existing SQL scripts to emphasize they are for MySQL. (2) Add new SQL scripts -- they are for PostgreSQL.

---
+++ data/sql/my-01-create-and-populate-table-attr_x0s.sql
@@ -0,0 +1,39 @@
+--
+-- data/sql/my-01-create-and-populate-table-attr_x0s.sql
+-- ============================================================================
+-- Reporter Multilang. Version 0.1
+-- ============================================================================
+-- A tool to generate human-readable reports based on data from various sources
+-- with the focus on its implementation using a series of programming languages
+-- ============================================================================
+-- Written by Radislav (Radicchio) Golubtsov, 2016
+--
+-- This is free and unencumbered software released into the public domain.
+--
+-- Anyone is free to copy, modify, publish, use, compile, sell, or
+-- distribute this software, either in source code form or as a compiled
+-- binary, for any purpose, commercial or non-commercial, and by any
+-- means.
+--
+-- (See the LICENSE file at the top of the source tree.)
+--
+
+show tables;
+
+create table attr_x0s (id          serial    primary key,
+                       name        character varying(64) not null,
+                       description character varying(1024));
+
+show tables;
+
+show columns in attr_x0s;
+
+select * from attr_x0s;
+
+insert into attr_x0s (name, description) values ('any',    'Any ==========');
+insert into attr_x0s (name, description) values ('i686',   'I686 =========');
+insert into attr_x0s (name, description) values ('x86_64', 'X86_64 =======');
+
+select * from attr_x0s;
+
+-- vim:set nu:et:ts=4:sw=4:
---
+++ data/sql/pg-02-create-and-populate-table-attr_x1s.sql
@@ -0,0 +1,42 @@
+--
+-- data/sql/pg-02-create-and-populate-table-attr_x1s.sql
+-- ============================================================================
+-- Reporter Multilang. Version 0.1
+-- ============================================================================
+-- A tool to generate human-readable reports based on data from various sources
+-- with the focus on its implementation using a series of programming languages
+-- ============================================================================
+-- Written by Radislav (Radicchio) Golubtsov, 2016
+--
+-- This is free and unencumbered software released into the public domain.
+--
+-- Anyone is free to copy, modify, publish, use, compile, sell, or
+-- distribute this software, either in source code form or as a compiled
+-- binary, for any purpose, commercial or non-commercial, and by any
+-- means.
+--
+-- (See the LICENSE file at the top of the source tree.)
+--
+
+\d
+
+create table attr_x1s (id          serial    primary key,
+                       name        character varying(64) not null,
+                       description character varying(1024));
+
+\d
+
+\d+ attr_x1s
+
+select * from attr_x1s;
+
+insert into attr_x1s (name, description) values ('core',  'Core ============');
+insert into attr_x1s (name, description) values ('extra', 'Extra ===========');
+insert into attr_x1s (name, description) values ('community',
+                                                          'Community =======');
+insert into attr_x1s (name, description) values ('multilib',
+                                                          'Multilib ========');
+
+select * from attr_x1s;
+
+-- vim:set nu:et:ts=4:sw=4:
