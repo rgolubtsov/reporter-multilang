eb14c1c31e3f5d9ecdde54026dcde3617ac0dee6
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2025-01-15 15:30:00 +0300

    Bump copyright year.

--- reporter-cli/sql-pdf/perl/src/ReporterPrimary/ReporterController.pm
+++ reporter-cli/sql-pdf/perl/src/ReporterPrimary/ReporterController.pm
@@ -6,7 +6,7 @@
 # A tool to generate human-readable reports based on data from various sources
 # with the focus on its implementation using a series of programming languages.
 # =============================================================================
-# Written by Radislav (Radicchio) Golubtsov, 2016-2024
+# Written by Radislav (Radicchio) Golubtsov, 2016-2025
 #
 # This is free and unencumbered software released into the public domain.
 #
